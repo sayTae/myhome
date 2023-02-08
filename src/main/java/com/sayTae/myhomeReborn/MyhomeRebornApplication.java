42afd3107dcbdd2103791f67e2b02f764196939d
taeyeong <mattam1129@gmail.com>
2023-02-08 18:37:00 +0900

    init

---
+++ src/main/java/com/sayTae/myhomeReborn/MyhomeRebornApplication.java
@@ -0,0 +1,13 @@
+package com.sayTae.myhomeReborn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyhomeRebornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyhomeRebornApplication.class, args);
+	}
+
+}
